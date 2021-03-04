33e6627c7c1870f5f217fc0fdddfb54bff8626f3
EvgeniiKarelin <eugenefolkast@gmail.com>
2021-03-04 19:43:13 +0300

    Method adding
    Finally fucking added to method fucking usefullnes

--- src/main/java/form/ConnectionForm.java
+++ src/main/java/form/ConnectionForm.java
@@ -1,16 +1,23 @@
 package form;
 
 import core.ParsMatches;
+import core.Write;
 import core.load.Load;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Properties;
@@ -219,17 +226,21 @@ public class ConnectionForm extends JFrame {
      * по нажатию на кнопку "Вывести информацию по спортсменам"
      *
      */
-    public void sportsmanWriteScore(){
+    public void sportsmanWriteScore() {
         FileDialog fd = new FileDialog(this, "Выберите файл...", FileDialog.LOAD);
         fd.setDirectory("C:\\");
         fd.setFile("*.xls");
         fd.setVisible(true);
-        String filename = fd.getFile();
-        if (filename == null)
+        String fileName = fd.getFile();
+        if (fileName == null)
             showMessageDialog(this,"Файл не выбран");
         else
-            showMessageDialog(this,"Вы выбрали "+filename);
-        
+            showMessageDialog(this,"Вы выбрали "+fileName);
+
+        Write w = new Write();
+        w.readXls(fileName);
+
     }
+
 
 }
